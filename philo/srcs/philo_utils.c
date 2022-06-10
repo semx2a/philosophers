4bdccb30e7b59084f9226a0b7366119b856c6a2c
Semiha OZCAN <seozcan@e1r5p15.42.fr>
2022-06-10 18:44:10 +0200

    [NEW] philo_util.c [NEW] ft_atoli function [NEW] ft_strlen function [NEW] ft_isdigit function [NEW] time_diff function [NEW] ft_error

---
+++ philo/srcs/philo_utils.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seozcan <seozcan@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/10 17:38:34 by seozcan           #+#    #+#             */
+/*   Updated: 2022/06/10 18:43:27 by seozcan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/philo.h"
+
+int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+
+long	ft_atoli(const char *str)
+{
+	int		i;
+	int		n;
+	long	r;
+
+	i = 0;
+	n = 1;
+	r = 0;
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == 43 || str[i] == 45)
+	{
+		if (str[i] == 45)
+			n *= -1;
+		i++;
+	}
+	while (ft_isdigit(str[i]))
+	{
+		r = r * 10 + str[i] - 48;
+		i++;
+	}
+	return (n * r);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	len;
+
+	len = 0;
+	if (str)
+		while (str[len])
+			len++;
+	return (len);
+}
+
+float	time_diff(struct timeval *start, struct timeval *end)
+{
+	return ((end->tv_sec - start->tv_sec)
+		+ 1e-6 * (end->tv_usec - start->tv_usec));
+}
+
+void	ft_error(char *str)
+{
+	write(2, str, ft_strlen(str));
+}

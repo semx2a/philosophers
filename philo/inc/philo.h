8529ae720720a52dbd385d9ea08360bd6cb564f9
Semiha OZCAN <seozcan@e1r10p5.42.fr>
2022-06-20 23:20:12 +0200

    [ADD]	<stdint.h> library to enable portable int/void cast 	ERR_THREAD error string 	MAX_THREADS param 	s_mutex struct to store mutexes 	s_time struct to store time values 	routine function prototype [MOD]	refactoring of s_main struct

--- philo/inc/philo.h
+++ philo/inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: seozcan <seozcan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 19:14:19 by seozcan           #+#    #+#             */
-/*   Updated: 2022/06/14 21:24:47 by seozcan          ###   ########.fr       */
+/*   Updated: 2022/06/20 23:18:06 by seozcan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,53 +33,52 @@
 //		pthread*
 # include <pthread.h>
 
+//		int/pointer cast
+# include <stdint.h>
+
 //		ERRORS
 # define ERR			"Error"
 # define ERR_DIGITS		"Error: only digits allowed."
 # define ERR_ARGS		"Error: only 4 or 5 arguments allowed."
 # define ERR_MAXINT		"Error: MAXINT detected."
+# define ERR_THREAD		"Philosopher died on inception."
 
 //		PARAMETERS
 # define INT_MIN		-2147483648
 # define INT_MAX		2147483647
-
-//		LINKED LISTS
-//		nodes structure
-typedef struct s_node
-{
-	void			*data;	
-	struct s_node	*prev;
-	struct s_node	*next;
-}	t_node;
-
-typedef struct s_lst
-{
-	t_node	*head;
-	t_node	*tail;
-}	t_lst;
+# define MAX_THREADS	1024
 
 //		STRUCTURES
-//		push_swap.c
+
+typedef struct s_mutex
+{
+	int				data;
+	pthread_mutex_t mutex;
+} 	t_mutex;
+
+typedef struct s_time
+{	
+	float			*time;
+	struct timeval	start;
+	struct timeval	end;
+}	t_time;
+
 typedef struct s_main
 {
 	int				i;
 	int				j;
-	int				k;
-	int				fd;
+	int				*ret;
 	int				philo_nb;
-	int				time_2die;
-	int				time_2eat;
-	int				time_2sleep;
-	int				u_eats;
-	int				size;
-	int				*arr;
-	int				**tab;
-	char			*str;
-	char			**stab;	
-	float			*time;
-	struct timeval	start;
-	struct timeval	end;
+	int				forks;
+	int				n_eats;
+	unsigned int	time_2die;
+	unsigned int	time_2eat;
+	unsigned int	time_2sleep;
+	pthread_t		*philosophers;
 }	t_main;
+
+//		main.c
+void	*routine(void *p_data);
 
 //		philo_init.c
 int		init_params(t_main *m, int ac, char **av);
